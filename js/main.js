a4597bb28efb56ff8dea17be8ae3d650f65e7667
ppopow <pawel.popow@gmail.com>
2020-10-17 23:36:57 +0200

    nowe zadanie 28

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = 26;
+let secondNumber = 37;
+
+function multiply(firstNumber, secondNumber) {
+    let result = firstNumber * secondNumber;
+    return result;
+}
+console.log( multiply(26, 37) );
